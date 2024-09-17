808c66dce553f28d6b8bacfdecb6c245ed68f02a
Deborah Mok <mokd@usc.edu>
2024-09-17 15:42:21 -0700

    blockProvider

--- lib/blockProvider.dart
+++ lib/blockProvider.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:testtrial/robot.dart';
-import 'package:testtrial/yahtzeePrep.dart';
-import 'package:testtrial/CalcDeborah.dart';
-import "package:testtrial/blockProvider.dart";
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(BlockchainApp());
+// Define a block in the blockchain
+class Block
+{
+  final int index;
+  final String previousHash;
+  final String data;
+  final String hash;
+
+  Block(this.index, this.previousHash, this.data)
+      : hash = _generateHash(index, previousHash, data);
+
+  static String _generateHash(int index, String previousHash, String data) {
+    return (index.toString() + previousHash + data).hashCode.toString();
+  }
+
+  @override
+  String toString() {
+    return 'Block $index: $hash (Previous: $previousHash)';
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// A simple blockchain structure to store blocks
+class Blockchain {
+  final List<Block> _chain = [];
 
-  // This widget is the root of your application.
+  Blockchain() {
+    // Genesis block
+    _chain.add(Block(0, '0', 'Genesis Block'));
+  }
+
+  void addBlock(String data) {
+    final previousBlock = _chain.last;
+    final newBlock = Block(
+      previousBlock.index + 1,
+      previousBlock.hash,
+      data,
+    );
+    _chain.add(newBlock);
+  }
+
+  void displayChain() {
+    for (var block in _chain) {
+      print(block.toString());
+    }
+  }
+}
+
+void main() {
+  final blockchain = Blockchain();
+
+  // Simulate storing the code as data within blocks
+  blockchain.addBlock('Flutter app code block 1');
+
+  // Display the blockchain in the console
+  blockchain.displayChain();
+
+  // Run the Flutter app
+  runApp(BlockProvider());
+}
+
+class BlockProvider extends StatelessWidget {
+  const BlockProvider({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
