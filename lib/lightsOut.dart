ac76923d0dd691df5aa6783a781ac6dceeff1eb0
Deborah Mok <mokd@usc.edu>
2024-09-24 18:25:25 -0700

    lightOutcode

--- lib/lightsOut.dart
+++ lib/lightsOut.dart
@@ -1,155 +1,126 @@
-import "dart:math";
-import "package:flutter/material.dart";
-import "package:flutter_bloc/flutter_bloc.dart";
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PanelState
-{
-  List<Light> panel = [];
+class PanelState {
+  final List<Light> panel;
 
-  PanelState( int n )
-  { panel = [];
-  for ( int i=0; i<n; i++ )
-  { panel.add(Light()); }
-  for ( int i=1; i<n-1; i++ )
-  { panel[i].addNeighbor(panel[i-1]);
-  panel[i].addNeighbor(panel[i+1]);
-  }
-  panel[0].addNeighbor(panel[1]);
-  panel[n-1].addNeighbor(panel[n-2]);
+  PanelState(int n) : panel = List.generate(n, (_) => Light()) {
+    for (int i = 1; i < n - 1; i++) {
+      panel[i].addNeighbor(panel[i - 1]);
+      panel[i].addNeighbor(panel[i + 1]);
+    }
+    if (n > 1) {
+      panel[0].addNeighbor(panel[1]);
+      panel[n - 1].addNeighbor(panel[n - 2]);
+    }
   }
 }
-class PanelCubit extends Cubit<PanelState>
-{
-  PanelCubit(int n) : super( PanelState(n) );
 
-  void update( int n ) { emit( PanelState(n) ); }
+class PanelCubit extends Cubit<PanelState> {
+  PanelCubit(int n) : super(PanelState(n));
+
+  void update(int n) => emit(PanelState(n));
 }
 
-void main() // 123
-{ runApp( LightsOut() );
+void main() {
+  runApp(LightsOut());
 }
 
-class LightsOut extends StatelessWidget
-{ LightsOut({super.key});
-@override
-Widget build( BuildContext context )
-{ return  MaterialApp
-  ( title: "LightsOut - Barrett",
-  home: LightsOutHome(),
-);
-}
-}
-
-class LightsOutHome extends StatelessWidget
-{
+class LightsOut extends StatelessWidget {
   @override
-  Widget build( BuildContext context )
-  { return Scaffold
-    ( appBar: AppBar(title: Text("Lights Out - Barrett") ),
-    body: LOBody(),
-  );
-  }
-}
-
-class LOBody extends StatelessWidget
-{
-  Widget build( BuildContext context )
-  { TextEditingController tec = TextEditingController();
-  tec.text="9";
-  return BlocProvider<PanelCubit>
-    ( create: (context) => PanelCubit(9),
-    child: BlocBuilder<PanelCubit,PanelState>
-      ( builder: (context, panelState)
-    { return Column
-      ( children:
-    [ Row( children:
-    BlocProvider.of<PanelCubit>(context).state.panel,
-    ),
-      TextField( controller: tec),
-      FloatingActionButton
-        ( onPressed: ()
-      { int n = int.parse(tec.text);
-      BlocProvider.of<PanelCubit>(context).update(n);
-      },
-        child: Text("new number of lights"),
-      ),
-    ],
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LightsOut',
+      home: LightsOutHome(),
     );
-    },
-    ),
-  );
   }
 }
 
-class LightState
-{
-  late bool isOn;
-
-  LightState( this.isOn );
-  LightState.random()
-  { Random randy = Random();
-  isOn = randy.nextBool();
-  }
-}
-class LightCubit extends Cubit<LightState>
-{
-  LightCubit( ) : super( LightState.random() );
-
-  void update( bool zon ) { emit( LightState(zon) ); }
-  void toggle() { emit( LightState(!state.isOn) ); }
-}
-
-class Light extends StatelessWidget
-{
-  final List<Light> neighbors = []; // ones on each side (or one side)
-
-  void addNeighbor( Light li ) { neighbors.add(li); }
-
-  late BuildContext bc;
+class LightsOutHome extends StatelessWidget {
+  final TextEditingController _tec = TextEditingController(text: '9');
 
   @override
-  Widget build( BuildContext context )
-  { return BlocProvider<LightCubit>
-    ( create: (context) => (LightCubit()),
-    child: BlocBuilder<LightCubit,LightState>
-      ( builder: (context, lightState)
-    { return Builder
-      ( builder: (context)
-    { LightCubit lc = BlocProvider.of<LightCubit>(context);
-    bc = context;
-    return Column
-      ( children:
-    [  Container
-      ( height: 60, width: 60,
-      decoration: BoxDecoration
-        ( border: Border.all(width:2),
-        color: lc.state.isOn
-            ? Colors.yellow: Colors.brown,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Lights Out')),
+      body: BlocProvider(
+        create: (context) => PanelCubit(9),
+        child: Column(
+          children: [
+            BlocBuilder<PanelCubit, PanelState>(
+              builder: (context, panelState) {
+                return Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: panelState.panel.map((light) {
+                    return light.build(context);
+                  }).toList(),
+                );
+              },
+            ),
+            TextField(
+              controller: _tec,
+              keyboardType: TextInputType.number,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                int n = int.parse(_tec.text);
+                BlocProvider.of<PanelCubit>(context).update(n);
+              },
+              child: Text("Update Lights"),
+            ),
+          ],
+        ),
       ),
-    ),
-      FloatingActionButton
-        ( onPressed: ()
-      { lc.update( !BlocProvider.of<LightCubit>(context).state.isOn );
-      for ( Light li in neighbors )
-      {
-        li.toggle();
-      }
-      },
-      ),
-    ],
     );
-    },
-    );
-
-    },
-    ),
-  );
-  }
-
-  void toggle()
-  {
-    LightCubit lc = BlocProvider.of<LightCubit>(bc);
-    lc.toggle();
   }
 }
 
+class LightState {
+  bool isOn;
+
+  LightState(this.isOn);
+  LightState.random() : isOn = Random().nextBool();
+}
+
+class LightCubit extends Cubit<LightState> {
+  LightCubit() : super(LightState.random());
+
+  void toggle() => emit(LightState(!state.isOn));
+}
+
+class Light {
+  final List<Light> neighbors = [];
+  final LightCubit _cubit = LightCubit();
+
+  void addNeighbor(Light neighbor) {
+    neighbors.add(neighbor);
+  }
+
+  Widget build(BuildContext context) {
+    return BlocProvider<LightCubit>.value(
+      value: _cubit,
+      child: BlocBuilder<LightCubit, LightState>(
+        builder: (context, lightState) {
+          return GestureDetector(
+            onTap: () {
+              _cubit.toggle();
+              for (var neighbor in neighbors) {
+                neighbor._cubit.toggle();
+              }
+            },
+            child: Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                border: Border.all(width: 2),
+                color: lightState.isOn ? Colors.yellow : Colors.brown,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
